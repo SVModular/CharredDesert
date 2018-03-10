777b0ae3b35ebcd77664725832a98f08ce3ecc71
Jerry Sievert <code@legitimatesounding.com>
2018-03-10 11:08:53 -0800

    update for pr

--- src/components/custom.hpp
+++ src/components/custom.hpp
@@ -2,7 +2,7 @@
 
 #include "rack.hpp"
 #include "asset.hpp"
-#include "components.hpp"
+#include "componentlibrary.hpp"
 
 extern Plugin *plugin;
 
@@ -18,36 +18,4 @@ struct Davies1900hRedSnapKnob : Davies1900hRedKnob {
   Davies1900hRedSnapKnob ( ) {
     snap = true;
   }
-};
-
-struct MotorWidget : TransparentWidget {
-	float diameter;
-	float *value;
-	bool *connected;
-
-	MotorWidget ( ) { }
-
-	void draw (NVGcontext *vg) {
-		if (*connected) {
-		  float xx = diameter * sin(-(*value * 0.17 + 0.15) * M_PI) ;
-		  float yy = diameter * cos((*value * 0.17 + 0.15) * M_PI) ;
-
-			nvgBeginPath(vg);
-			nvgCircle(vg, 0.0f, 0.0f, diameter);
-			nvgFillColor(vg, nvgRGBA(0xf3, 0x00, 0x00, 0xff));
-			nvgFill(vg);
-
-			nvgStrokeWidth(vg, 1.2f);
-
-			nvgStrokeColor(vg, nvgRGBA(0xff, 0xff, 0xff, 0xff));
-			{
-				nvgBeginPath(vg);
-				nvgMoveTo(vg, 0, 0);
-				nvgLineTo(vg, xx, yy);
-				nvgClosePath(vg);
-			}
-
-			nvgStroke(vg);
-		}
-	}
 };
