9c991c5b163acaeb682b8405a542a5197adcfdfc
Jerry Sievert <code@legitimatesounding.com>
2018-08-03 16:09:26 -0700

    some more catbon cleanup

--- src/controller/Carbon.cpp
+++ src/controller/Carbon.cpp
@@ -2,12 +2,15 @@
 
 CarbonModule::CarbonModule()
     : Module(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS) {
+  frequency = 0.0f;
 }
 
 void CarbonModule::step() {
-  if (inputs[AUDIO_INPUT].active) {
+  // update the display no matter what
+  frequency = clamp((inputs[FREQ_INPUT].active ? inputs[FREQ_INPUT].value * 1000 : 0) + params[FREQ_PARAM].value, 0.0f, 7000.0f);
+
+  if (inputs[AUDIO_INPUT].active && outputs[AUDIO_OUTPUT].active) {
     float audio_in = inputs[AUDIO_INPUT].value / 5.0f;
-    frequency = clamp((inputs[FREQ_INPUT].active ? inputs[FREQ_INPUT].value * 1000 : 0) + params[FREQ_PARAM].value, 0.0f, 7000.0f);
     float res = clamp((inputs[REZ_INPUT].active ? inputs[REZ_INPUT].value / 10 : 0) + params[REZ_PARAM].value, 0.0f, 4.0f);
 
     filter.setSamplerate(engineGetSampleRate());
